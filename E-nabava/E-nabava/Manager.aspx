d9b355169160f313057c879a4c82e17557be033a
tomislav-hop <tomislav.hop@gmail.com>
2015-05-09 13:18:23 +0200

    Inicijalni Commit

---
+++ E-nabava/E-nabava/Manager.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Manager.aspx.cs" Inherits="E_nabava.Manager" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 165px;
+            width: 498px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" BackColor="White" BorderColor="#E7E7FF" BorderStyle="None" BorderWidth="1px" CellPadding="3" DataKeyNames="id_korisnici" DataSourceID="SqlDataSourceKorisnici" GridLines="Horizontal">
+            <AlternatingRowStyle BackColor="#F7F7F7" />
+            <Columns>
+                <asp:BoundField DataField="id_korisnici" HeaderText="id_korisnici" ReadOnly="True" SortExpression="id_korisnici" />
+                <asp:BoundField DataField="naziv_dobavljaca" HeaderText="naziv_dobavljaca" SortExpression="naziv_dobavljaca" />
+                <asp:BoundField DataField="adresa_dobavljaca" HeaderText="adresa_dobavljaca" SortExpression="adresa_dobavljaca" />
+                <asp:BoundField DataField="email" HeaderText="email" SortExpression="email" />
+                <asp:BoundField DataField="broj_telefona" HeaderText="broj_telefona" SortExpression="broj_telefona" />
+                <asp:BoundField DataField="korisnicko_ime" HeaderText="korisnicko_ime" SortExpression="korisnicko_ime" />
+                <asp:BoundField DataField="lozinka" HeaderText="lozinka" SortExpression="lozinka" />
+            </Columns>
+            <FooterStyle BackColor="#B5C7DE" ForeColor="#4A3C8C" />
+            <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#F7F7F7" />
+            <PagerStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" HorizontalAlign="Right" />
+            <RowStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" />
+            <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="#F7F7F7" />
+            <SortedAscendingCellStyle BackColor="#F4F4FD" />
+            <SortedAscendingHeaderStyle BackColor="#5A4C9D" />
+            <SortedDescendingCellStyle BackColor="#D8D8F0" />
+            <SortedDescendingHeaderStyle BackColor="#3E3277" />
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSourceKorisnici" runat="server" ConnectionString="<%$ ConnectionStrings:KorisniciConnectionString %>" OnSelecting="SqlDataSource1_Selecting" SelectCommand="SELECT * FROM [Korisnici]"></asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ E-nabava/E-nabava/Registration.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace E_nabava
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string JQueryVer = "1.7.1";
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
+            {
+                Path = "~/Scripts/jquery-" + JQueryVer + ".min.js",
+                DebugPath = "~/Scripts/jquery-" + JQueryVer + ".js",
+                CdnPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-" + JQueryVer + ".min.js",
+                CdnDebugPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-" + JQueryVer + ".js",
+                CdnSupportsSecureConnection = true,
+                LoadSuccessExpression = "window.jQuery"
+            });
+            if (IsPostBack) {
+                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["KorisniciConnectionString"].ConnectionString);
+                conn.Open();
+                string provjeriKorisnika="select count(*) from Korisnici where korisnicko_ime='"+tbKorisnickoIme.Text+"'";
+                SqlCommand comm = new SqlCommand(provjeriKorisnika,conn);
+                int temp  =   Convert.ToInt32(comm.ExecuteScalar().ToString());
+                if(temp==1){
+                Response.Write("Korisnicko ime je zauzeto!");
+                }
+                conn.Close();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+             SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["KorisniciConnectionString"].ConnectionString);
+               try{
+                   Guid guid = Guid.NewGuid();  
+                conn.Open();
+                string insertQuerry="insert into  Korisnici (id_korisnici,naziv_dobavljaca,adresa_dobavljaca,email,broj_telefona,korisnicko_ime,lozinka) values (@Id,@Naziv,@Adresa,@Email,@Broj,@Korime,@Lozinka)";
+                SqlCommand comm = new SqlCommand(insertQuerry,conn);
+                comm.Parameters.AddWithValue("@Id", guid);
+               comm.Parameters.AddWithValue("@Naziv",tbNaziv.Text);
+               comm.Parameters.AddWithValue("@Adresa",tbAdresa.Text);
+               comm.Parameters.AddWithValue("@Email",tbEmail.Text);
+               comm.Parameters.AddWithValue("@Broj",tbBrojTelefona.Text);
+               comm.Parameters.AddWithValue("@Korime",tbKorisnickoIme.Text);
+               comm.Parameters.AddWithValue("@Lozinka",tbLozinka.Text);
+               comm.ExecuteNonQuery();
+               Response.Redirect("Manager.aspx");
+               Response.Write("Registracija uspjesna");
+
+                conn.Close();
+               }
+            catch(Exception ex){
+            Response.Write("Error:"+ ex.ToString());
+            }
+          
+        }
+    }
+}
