594790e7e3efb02a7cb1e23ab708752ac0d56202
Nikola Šamonik <nsamonik@foi.hr>
2015-05-09 13:34:56 +0200

    v1.3.

---
+++ E-nabava/E-nabava/Login.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="E_nabava.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+        .auto-style2 {
+            width: 100%;
+        }
+        .auto-style3 {
+            width: 172px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <strong>Prijava</strong></div>
+        <table class="auto-style2">
+            <tr>
+                <td class="auto-style3" style="text-align: right">Username</td>
+                <td>
+                    <asp:TextBox ID="tbUname" runat="server"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="tbUname" ErrorMessage="Korisnicko ime je obavezno"></asp:RequiredFieldValidator>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style3" style="text-align: right">Password</td>
+                <td>
+                    <asp:TextBox ID="tbPass" runat="server" TextMode="Password"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="tbPass" ErrorMessage="Lozinka je obavezna"></asp:RequiredFieldValidator>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style3">&nbsp;</td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Login" Width="125px" />
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style3">&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ E-nabava/E-nabava/Login.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+namespace E_nabava
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string JQueryVer = "1.7.1";
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
+            {
+                Path = "~/Scripts/jquery-" + JQueryVer + ".min.js",
+                DebugPath = "~/Scripts/jquery-" + JQueryVer + ".js",
+                CdnPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-" + JQueryVer + ".min.js",
+                CdnDebugPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-" + JQueryVer + ".js",
+                CdnSupportsSecureConnection = true,
+                LoadSuccessExpression = "window.jQuery"
+            });
+            if (IsPostBack)
+            {
+                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["KorisniciConnectionString"].ConnectionString);
+                conn.Open();
+                string provjeriKorisnika = "select count(*) from Korisnici where korisnicko_ime='" + tbUname.Text + "'";
+                SqlCommand comm = new SqlCommand(provjeriKorisnika, conn);
+                int temp = Convert.ToInt32(comm.ExecuteScalar().ToString());
+                conn.Close();
+                if (temp == 1)
+                {
+                    conn.Open();
+                    string passwordQuery = "select lozinka from Korisnici where korisnicko_ime='" + tbUname.Text + "'";
+                    SqlCommand passComm = new SqlCommand(passwordQuery, conn);
+                    string password = passComm.ExecuteScalar().ToString().Replace(" ","");
+                    if (password == tbPass.Text)
+                    {
+                        Session["New"] = tbUname.Text;
+                        Response.Write("Password is correct!");
+                        Response.Redirect("Manager.aspx");
+                    }
+                    else
+                    {
+                        Response.Write("Password is incorrect!");
+                    }
+                }
+                else {
+                    Response.Write("Username is incorrect!");
+                }
+                conn.Close();
+            }
+        }
+    }
+}
